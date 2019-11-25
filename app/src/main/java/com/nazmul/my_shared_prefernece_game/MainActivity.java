f4cdba208c8ff7f2b9b3390be206e11d8b5bb177
Md.Nazmul Alom <34202639+CoderNazmulAlom@users.noreply.github.com>
2019-11-25 17:00:10 +0600

    Initial commit

---
+++ app/src/main/java/com/nazmul/my_shared_prefernece_game/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nazmul.my_shared_prefernece_game;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
